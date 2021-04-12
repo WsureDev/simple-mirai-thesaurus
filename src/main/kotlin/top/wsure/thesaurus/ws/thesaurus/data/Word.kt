8baa36c90b97d09230de51dc13cf17b20e5152e3
Wsure <root@wsure.top>
2021-04-12 09:59:27 +0800

    add

---
+++ src/main/kotlin/top/wsure/thesaurus/ws/thesaurus/data/Word.kt
@@ -0,0 +1,11 @@
+package top.wsure.thesaurus.ws.thesaurus.data
+
+import top.wsure.thesaurus.ws.thesaurus.enums.MessageType
+
+data class Word(
+    var question:String,
+    var answer:String,
+    var type:MessageType,
+){
+    constructor(question:String,answer:String,type: Int):this(question,answer, MessageType.valueOf(type)!!)
+}
